1237ed950a9236195d0d00ec24db0d19721bdfa5
Rewdog <rew@rewdog.com>
2025-04-25 19:03:49 -0400

    Update root index.js with redirection comment

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-// Placeholder entry-point file for Cloudflare Workers
-export default {
-  async fetch(request) {
-    return new Response("Hello from Cloudflare Workers!", {
-      headers: { "content-type": "text/plain" },
-    });
-  },
-};
+// This file is deprecated and kept only for reference.
+// The actual Workers entry point is now in the workers-site/index.js file.
+// Please refer to that file for any modifications.
+
+// This file will be removed in a future update.

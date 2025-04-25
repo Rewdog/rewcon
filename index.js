e50883f0c76f1e83716cb217d95a3fc489bd4807
Rewdog <rew@rewdog.com>
2025-04-25 19:26:35 -0400

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-// This file is deprecated and kept only for reference.
-// The actual Workers entry point is now in the workers-site/index.js file.
-// Please refer to that file for any modifications.
-
-// This file will be removed in a future update.

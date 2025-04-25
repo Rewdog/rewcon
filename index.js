12b42bd2a3482b3d881684a8474b46103eb55ad4
Rewdog <rew@rewdog.com>
2025-04-25 19:20:35 -0400

    cleanup

--- index.js
+++
@@ -1,8 +0,0 @@
-// Placeholder entry-point file for Cloudflare Workers
-export default {
-  async fetch(request) {
-    return new Response("Hello from Cloudflare Workers!", {
-      headers: { "content-type": "text/plain" },
-    });
-  },
-};
